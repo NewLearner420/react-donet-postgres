f1a86b8291f1b68bcc729549cb3de01cf76927ac
NewLearner420 <er.krishankumar@yahoo.com>
2025-11-22 07:17:09 +0000

    Debug PORT variable and fix default

--- keycloak/entrypoint.sh
+++ keycloak/entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 echo "Starting Keycloak with Neon PostgreSQL..."
+echo "PORT env variable is: ${PORT}"
 
 exec /opt/keycloak/bin/kc.sh start \
   --optimized \
   --http-host=0.0.0.0 \
-  --http-port=${PORT:-8080} \
+  --http-port="${PORT:-10000}" \
   --hostname-strict=false \
   --http-enabled=true \
   --proxy=edge
