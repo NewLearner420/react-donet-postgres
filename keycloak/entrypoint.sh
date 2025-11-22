cf0b49e918566565aff52c3d8928d7018e7a5c1b
NewLearner420 <er.krishankumar@yahoo.com>
2025-11-22 12:27:36 +0000

    Fix 1

--- keycloak/entrypoint.sh
+++ keycloak/entrypoint.sh
@@ -3,8 +3,10 @@ set -e
 
 echo "Starting Keycloak with Neon PostgreSQL..."
 echo "PORT env variable is: ${PORT}"
-echo "KC_DB_URL is: ${KC_DB_URL}"
-echo "KC_DB_USERNAME is: ${KC_DB_USERNAME}"
+
+# Optional: wait for database to be ready
+echo "Waiting for database connection..."
+sleep 5
 
 exec /opt/keycloak/bin/kc.sh start \
   --optimized \
@@ -12,4 +14,5 @@ exec /opt/keycloak/bin/kc.sh start \
   --http-port="${PORT:-10000}" \
   --hostname-strict=false \
   --http-enabled=true \
-  --proxy=edge
+  --proxy=edge \
+  --health-enabled=true
